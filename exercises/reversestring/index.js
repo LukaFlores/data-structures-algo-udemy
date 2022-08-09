bd148437baeafead3c1e44244dc8211f92b90d78
Luka Flores <lukaflores@Lukas-MacBook-Pro.local>
2022-08-09 12:44:43 -0700

    exercise: reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse1(str) {
+  return str.split("").reverse().join("");
+}
 
-module.exports = reverse;
+function reverse2(str) {
+  let reversed = "";
+
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+
+
+function reverse3(str) {
+  return str.split('').reduce(( reversed, character) => {
+    return character + reversed 
+  }, '')  
+}
+
+module.exports = reverse1;
