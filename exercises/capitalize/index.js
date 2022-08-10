ca3c56080e3df810d36f768a7d43c6f1f3bcfb02
Luka Flores <luka.flores@mail.com>
2022-08-10 10:01:47 -0700

    exercise: capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,25 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const arr = [];
+  str.split(" ").forEach((element) => {
+    arr.push(element[0].toUpperCase() + element.slice(1));
+  });
+  return arr.join(" ");
+}
 
-module.exports = capitalize;
+function capitalize1(str) {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+module.exports = capitalize1;
